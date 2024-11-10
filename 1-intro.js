c38895b56d918c20d702dc0089dfddcb8990975c
nanakaz3 <doclemandarin@gmail.com>
2024-11-10 12:42:05 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,13 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("small number");
+} else {
+  console.log("large number");
+}
+console.log(`hey it'is my first node app!!!`);
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("Hello World");
+}, 1000);
---
+++ 10-fs-sync.js
@@ -0,0 +1,14 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+console.log(first, second);
+
+// To append the files:
+
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result ${first}, ${second}`,
+  { flag: "a" }
+);
